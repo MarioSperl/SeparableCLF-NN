ec8e2e8c6ae0f56e5fd65656d02a562f199e636f
Mario Sperl <mario.sperl@uni-bayreuth.de>
2024-08-10 10:35:51 +0200

    clean up in examples

--- examples.py
+++ examples.py
@@ -1,14 +1,26 @@
 import numpy as np
 import tensorflow as tf
-import random 
-
 
 class CLF_10dim:
+    """
+    A class representing a 10-dimensional system \dot{x} = f(x,u) = vf(x) + vg(x) u  
+
+    Methods:
+        
+        vf(x):
+            Computes the vector field vf(x) of the system at a given state `x`.
+
+        vg(x):
+            Computes the control influence matrix vg(x) at a given state `x`.
+
+        f(x, u):
+            Computes the system dynamics f(x,u) with control input `u` at state `x`.
+    """
     title = 'CLF_10dim'
 
     # define the dimensions of the problem
     inputdim = 10  # dimension of the state space (variable n in paper)
-    interval_size = 1  # compute a Lyapunov function on the cube [-interval_size, interval_size]**inputdim (variable C in paper)
+    interval_size = 1  # compute a Lyapunov function on the cube [-interval_size, interval_size]**inputdim
     control_size = 1  # assume the space of control values to be given as U = [-control_size, control_size]
     controldim = 5
     perturbation = np.random.normal(scale=0.1, size=(10, 10))
@@ -16,13 +28,18 @@ class CLF_10dim:
     T_inv = np.linalg.inv(T_matrix)
 
     def __init__(self):
-        self.constant_size = 4.0  
+        self.constant_size = 4.0  # transform the state space from [-constant_size, constant_size]**inputdim to [-1,1]**inputdim
 
-    # ------------------------------------------------------------------------------
-
-
-    # define the vector field
     def vf(self, x):
+        """
+        Define the vector field vf.
+        
+        Parameters:
+        x: Tensor of shape (batch_size, inputdim) representing the state
+        
+        Returns:
+        Array of shape (inputdim, batch_size) representing the vector field f(x)
+        """
         x = np.array(np.matmul(x, self.T_matrix.T))
         
         y = [- self.constant_size * x[:,0] + self.constant_size * x[:,1] * self.constant_size * x[:,0] - 0.1 * np.power(x[:, 8], 2), 0 * x[:,1], 
@@ -35,6 +52,15 @@ class CLF_10dim:
     
 
     def vg(self, x):
+        """
+        Define the control vector field vg.
+        
+        Parameters:
+        x: Tensor of shape (batch_size, inputdim) representing the state
+        
+        Returns:
+        Array of shape (inputdim, controldim, batch_size) representing the control vector field g(x)
+        """
         x = np.array(np.matmul(x, self.T_matrix.T))
 
         x1 = x[:, 1]
@@ -52,6 +78,16 @@ class CLF_10dim:
         return np.einsum('mnj,ni->mij', res, self.T_inv.T)
     
     def f(self, x, u):
+        """
+        Define the right-hand side function f.
+        
+        Parameters:
+        x: Tensor of shape (batch_size, inputdim) representing the state
+        u: Tensor of shape (batch_size, controldim) representing the control 
+        
+        Returns:
+        Tensor of shape (inputdim, batch_size) representing the r.h.s f(x,u)
+        """
         x = np.array(np.matmul(x, self.T_matrix.T))
 
         y = [- self.constant_size * x[:,0] + self.constant_size * x[:,1] * self.constant_size * x[:,0] - 0.1 * np.power(x[:, 8], 2),- self.constant_size * x[:,1] * u[:, 0], 
@@ -68,7 +104,7 @@ class CLF_10dim:
 
 class Pendulum:
     """
-    Class representing a 2-dimensional pendulum example.
+    Class representing a 2-dimensional pendulum example \dot{x} = f(x,u) = vf(x) + vg(x) u.
     """
 
     # Class constants
@@ -80,30 +116,44 @@ class Pendulum:
 
     def vf(self, x):
         """
-        Define the vector field \dot(x) = f(x) + g(x) u.
+        Define the vector field vf.
         
         Parameters:
         x: Tensor of shape (batch_size, inputdim) representing the state
         
         Returns:
-        Tensor of shape (batch_size, inputdim) representing the vector field f(x)
+        Array of shape (inputdim, batch_size) representing the vector field f(x)
         """
         x1 = x[:, 0]
         x2 = x[:, 1]
-        return [x2, -x2 + tf.sin(x1)]
+        return np.array([x2, -x2 + tf.sin(x1)]) 
 
     def vg(self, x):
         """
-        Define the control vector field g(x).
+        Define the control vector field vg.
         
         Parameters:
         x: Tensor of shape (batch_size, inputdim) representing the state
         
         Returns:
-        Tensor of shape (batch_size, inputdim, controldim) representing the control vector field g(x)
+        Array of shape (inputdim, controldim, batch_size) representing the control vector field g(x)
         """
         x1 = x[:, 0]
         x2 = x[:, 1]
-        return [[0 * x1, 1 + 0 * x2]]
+        return np.array([[0 * x1, 1 + 0 * x2]]) 
     
-    
+    def f(self,x,u):
+        """
+        Define the right-hand side function f.
+        
+        Parameters:
+        x: Tensor of shape (batch_size, inputdim) representing the state
+        u: Tensor of shape (batch_size, controldim) representing the control 
+        
+        Returns:
+        Tensor of shape (inputdim, batch_size) representing the r.h.s f(x,u)
+        """
+        x1 = x[:, 0]
+        x2 = x[:, 1]
+        res = [x2, -x2 + tf.sin(x1)+u[:,0]]
+        return tf.convert_to_tensor(res,  dtype=tf.float32)
